e3863a9b3dd1a4368392725a1bf554b9108ec2c6
Pallavi2911 <>
2023-02-21 14:45:05 +0900

    adding validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
 
-  ngOnInit(){
+  ngOnInit() {
     this.signupForm = new FormGroup({
-      'username' : new FormControl(null),
-      'email' : new FormControl(null),
-      'gender' : new FormControl('male'),
-    })
+      //The required validator don't have parenthesis after it because you don't want to execute this method.
+      //It is a static method made available by validators here.Instead, you only want to pass the reference to this method.
+      //Angular will execute the method whenever it detects that the input of this form control changed.
+      //So it just needs to have a reference on what it should execute at this point of time.So this is the required validator.
+      username: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      gender: new FormControl('male'),
+    });
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.signupForm);
   }
 }
