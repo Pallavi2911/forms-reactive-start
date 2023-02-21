5c75298dab334efeb22a54f1ceb0d8150e5cd3d8
Pallavi2911 <>
2023-02-21 17:28:16 +0900

    using error codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUserNames = ['abc', 'def'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
@@ -17,7 +18,10 @@ export class AppComponent implements OnInit {
         //It is a static method made available by validators here.Instead, you only want to pass the reference to this method.
         //Angular will execute the method whenever it detects that the input of this form control changed.
         //So it just needs to have a reference on what it should execute at this point of time.So this is the required validator.
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
@@ -36,5 +40,14 @@ export class AppComponent implements OnInit {
 
   getControls() {
     return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  //A validator in the end is just a function which gets executed by angular automatically
+  //when it checks the validity of the form control and it checks that validity whenever you change that control.
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
   }
 }
